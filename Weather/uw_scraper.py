eb885320b23b72bbf45b15fe3ae78b4881aa93e4
skipper8 <mharper48@gmail.com>
2021-05-24 11:04:20 -0700

    update

--- Weather/uw_scraper.py
+++ Weather/uw_scraper.py
@@ -6,11 +6,14 @@
 
 #IMPORTS___________________________________________________
 from bs4 import BeautifulSoup
+import requests
 
 class uw_scraper():
     def __init__(self):
-        self.URL= 'https://a.atmos.washington.edu/data/data.php?loc=local'
-        self.soup = BeautifulSoup(self.URL, 'html.parser')
+        self.URL= 'https://a.atmos.washington.edu/cgi-bin/list_uw.cgi'
+        self.soup = BeautifulSoup(requests.get(self.URL).content, 'html.parser')
     
-    def gather(self, rate):
-        
+    def gather(self):
+        latest = self.soup.find('b')
+        new_url = latest.a
+        return  new_url.get('href')
--- Weather/weather.py
+++ Weather/weather.py
@@ -2,3 +2,10 @@
 
 #GOAL:
 #A weather predictor model that can guess the weather of a particular locationup to 14 days in advance with an 80% accuracy
+
+#IMPORTS
+from uw_scraper import uw_scraper
+
+#test scraper
+uw = uw_scraper()
+print(uw.gather())
